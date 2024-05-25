239002a1ce60a911a2129a1f063d9861448251a9
Antônio M. Quadros Filho <antoniomquadrosfilho@gmail.com>
2024-05-25 20:16:29 -0300

    docs: add @link tag for doc comment

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * @package  DocumentTranslator
  * @author   Antônio M. Quadros Filho <antoniomquadrosfilho@gmail.com>
  * @license  [7](http://www.php.net/license/3_01.txt)  Licença PHP 3.01
- * @link     [8](http://pear.php.net/package/PackageName)
+ * @link     [8](https://packagist.org/packages/amqf/document-translator)
  */
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
